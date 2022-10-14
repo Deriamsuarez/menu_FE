3c0fa88599bfd7ad0f6d4340cb61bdb1c2bd317a
Deriamsuarez <45581669+Deriamsuarez@users.noreply.github.com>
2022-10-14 17:41:52 -0400

    f

--- src/views/Admin/Products/index.jsx
+++ src/views/Admin/Products/index.jsx
@@ -1,9 +1,18 @@
+import { Box, Divider, Fab, Stack, Typography } from '@mui/material'
 import React from 'react'
+import ProductTable from './components/ProductTable'
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
+    return (
+        <Stack spacing={4}>
+            <Typography variant='h4'><strong>Productos</strong></Typography>
+            <ProductTable />
+            <Fab position= "fixed" color="primary" aria-label="add" sx={{position: 'absolute', right: 50, bottom: 50}}>
+                <AddOutlinedIcon />
+            </Fab>
+        </Stack>
+    )
 }
 
 export default Products
--- src/views/Admin/Profiles/index.jsx
+++ src/views/Admin/Profiles/index.jsx
@@ -2,13 +2,12 @@ import { Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const Profiles = () => {
-  return (
-    <Stack>
-                <Typography variant='h4'><strong>Perfiles</strong></Typography>
-
-                <div>Profiles</div>
-    </Stack>
-  )
+    return (
+        <Stack>
+            <Typography variant='h4'><strong>Perfiles</strong></Typography>
+            <div>Profiles</div>
+        </Stack>
+    )
 }
 
 export default Profiles
